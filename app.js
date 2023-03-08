b97154356d9f32a9243f270831d721453394f454
221B0825 <eunseoyu0825@gmail.com>
2023-03-08 18:40:13 +0900

    use mouseEvents

--- app.js
+++ app.js
@@ -7,22 +7,30 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
 
-const colors = [
-  '#ff3838',
-  '#ffb8b8',
-  '#c56cf0',
-  '#ff9f1a',
-  '#fff200',
-  '#32ff7e',
-  '#7efff5',
-];
+let isPainting = false;
 
-function onclick(event) {
-  ctx.beginPath();
-  ctx.moveTo(400, 400);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+// move a single line: Alt + Direction Key
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  } else {
+    ctx.moveTo(event.offsetX, event.offsetY);
+  }
 }
-canvas.addEventListener('mousemove', onclick);
+
+function startPainting() {
+  isPainting = true;
+}
+
+function cancelPainting() {
+  isPainting = false;
+}
+
+//copy a single line: Shift + Alt + Direction Key
+//delete line: Ctrl + Shift + K
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', cancelPainting);
+canvas.addEventListener('mouseleave', cancelPainting);
