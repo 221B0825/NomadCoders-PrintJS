4a99a641395c1dda52bd87774ceff20fa730f3d0
221B0825 <eunseoyu0825@gmail.com>
2023-03-08 19:28:06 +0900

    add color box

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const lineWidth = document.getElementById('line-width');
 const color = document.getElementById('color');
+const colorOptions = Array.from(
+  document.getElementsByClassName('color-option')
+);
 const canvas = document.querySelector('canvas');
 
 //context
@@ -38,6 +41,12 @@ function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
 }
+//console.dir() : check the clicked object
+function onColorClick(event) {
+  ctx.strokeStyle = event.target.dataset.color;
+  ctx.fillStyle = event.target.dataset.color;
+  color.value = event.target.dataset.color;
+}
 
 //copy a single line: Shift + Alt + Direction Key
 //delete line: Ctrl + Shift + K
@@ -47,3 +56,6 @@ canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
 lineWidth.addEventListener('change', onLineWidthChange);
 color.addEventListener('change', onColorChange);
+
+//in this function, color = Each color option.
+colorOptions.forEach((color) => color.addEventListener('click', onColorClick));
